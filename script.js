530347f2012a5542593906c222b7a30ef5d4c828
ALKorzh <alexkorzhwork@gmail.com>
2024-03-07 20:20:36 +0300

    add func: createBomb, createMatrix

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-const FIELD_SIZE = document.querySelector(".input-size")
+const FIELD_SIZE = +document.querySelector(".input-size")
+const restart = document.querySelector(".restart-button")
+const start = document.querySelector(".start-button")
+const field = document.querySelector(".field")
+
+let matrix = []
+function createMatrix(FIELD_SIZE) {
+  matrix = Array.from(
+    { length: FIELD_SIZE },
+    (el) => (el = Array.from({ length: FIELD_SIZE }, (i) => (i = 0)))
+  )
+}
+
+function giveRandomNumber(endPosition) {
+  return Math.floor(Math.random() * endPosition)
+}
+function createBomb(FIELD_SIZE) {
+  const NUMBER_OF_BOMBS = Math.round(FIELD_SIZE / 4)
+
+  const bombPositions = new Set()
+  for (let i = 0; NUMBER_OF_BOMBS > bombPositions.size; i++) {
+    console.log(i)
+    bombPositions.add(giveRandomNumber(FIELD_SIZE ** 2))
+  }
+  return bombPositions
+}
+
+console.log(createBomb(6))
